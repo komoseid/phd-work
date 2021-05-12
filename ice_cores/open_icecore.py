a38809ac5059bef7541d22abeacf9ef9f05123a8
Kristine Onsum Moseid <krisomos@login-2.betzy.sigma2.no>
2021-05-12 13:07:11 +0200

    find model concentration from ice core locations

---
+++ ice_cores/open_icecore.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 11:54:13 2021
+sshfs krisomos@login.nird.sigma2.no:/scratch/krisomos/100121/ scratch/
+This script shall open and read the "new" concentration spreadsheets I got from Joe McConnell and Anja Eichler
+ppb = ug/l = ng/g
+@author: kristineom
+"""
+import pandas as pd
+import os,glob
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+import sys
+import warnings
+warnings.simplefilter("ignore")
+os.environ['HDF5_USE_FILE_LOCKING']='FALSE'
+
+def find_3x3matrix(var_ctrl, lati, long,arealook=False):
+
+    #find closest grid point
+    closest = var_ctrl.sel(lat=lati,lon=long,method='nearest')
+    #find indices of closest grid point
+    index = np.where((var_ctrl.lat == closest.lat.values) & (var_ctrl.lon == closest.lon.values))      
+    latind = index[0]
+    lonind = index[1]   
+    
+    #find surrounding grid points
+    point11 = var_ctrl.isel(lat=latind+1,lon=lonind+1)
+    point10 = var_ctrl.isel(lat=latind+1,lon=lonind)
+    point1_1 = var_ctrl.isel(lat=latind+1,lon=lonind-1)
+
+    point_11 = var_ctrl.isel(lat=latind-1,lon=lonind+1)
+    point_10 = var_ctrl.isel(lat=latind-1,lon=lonind)
+    point_1_1 = var_ctrl.isel(lat=latind-1,lon=lonind-1)
+    
+    point01 = var_ctrl.isel(lat=latind,lon=lonind+1)
+    point00 = var_ctrl.isel(lat=latind,lon=lonind)    #techniqally this is the same as closest, but like do include it here for clarity
+    point0_1 = var_ctrl.isel(lat=latind,lon=lonind-1)
+    
+    if arealook==True:
+        #print(point11)
+        #print(point11.squeeze()+point10.squeeze()+point1_1.squeeze()+point_11.squeeze()+point_10.squeeze()+point_1_1.squeeze()+point01.squeeze()+point00.squeeze()+point0_1.squeeze())
+        meanval = (point11.squeeze()+point10.squeeze()+point1_1.squeeze()+point_11.squeeze()+point_10.squeeze()+point_1_1.squeeze()+point01.squeeze()+point00.squeeze()+point0_1.squeeze())
+        outfile = meanval
+    else:
+        point11 = point11.groupby('time.year').mean().squeeze()#*(1E6/3.17098E-8)
+        point10 = point10.groupby('time.year').mean().squeeze()#*(1E6/3.17098E-8)
+        point1_1 = point1_1.groupby('time.year').mean().squeeze()#*(1E6/3.17098E-8)
+    
+        point_11 = point_11.groupby('time.year').mean().squeeze()#*(1E6/3.17098E-8)
+        point_10 = point_10.groupby('time.year').mean().squeeze()#*(1E6/3.17098E-8)
+        point_1_1 =point_1_1.groupby('time.year').mean().squeeze()#*(1E6/3.17098E-8)
+        
+        point01 = point01.groupby('time.year').mean().squeeze()#*(1E6/3.17098E-8)
+        point00 = point00.groupby('time.year').mean().squeeze()#*(1E6/3.17098E-8)
+        point0_1 =point0_1.groupby('time.year').mean().squeeze()#*(1E6/3.17098E-8)
+        
+        #meanval = (point11+point10+point1_1+point_11+point_10+point_1_1+point01+point00+point0_1)/9
+        meanval = (point11+point10+point1_1+point_11+point_10+point_1_1+point01+point00+point0_1)  #ONLY USE THIS IF YOU ARE DOING ug/g
+  
+        outfile = xr.DataArray(meanval, coords=[point11.year],dims="year")
+    return outfile
+
+"""
+# THIS FUNCTIONS DOES NOT WORK AS NEEDED YET - FIX LATER
+def dms2deg(coord):
+    import re
+    regex = re.compile("(\d+)°(\d+)'(?:(\d+)'')?([NESW])")
+    #lat = "45°55'55''N"
+    print(regex.match(coord).groups())
+    #lat = '''51°36'9.18"N'''
+    #deg, minutes, seconds, direction =  regex.match(coord).groups()
+    decimalcoord = (float(deg) + float(minutes)/60 + float(seconds)/(60*60)) * (-1 if direction in ['W', 'S'] else 1)
+    return decimalcoord
+"""
+
+def find_eichler_icecore(name, var):
+    # load data from ice core spreadsheet sent from Anja Eichler,
+    # this includes Colle Gnifetti, Belukha, Lomonosovfonna and Illimani
+    obs = pd.ExcelFile('/cluster/home/krisomos/ice_cores/data/Sulfat_BC_ice_cores_Eichler.xlsx')
+    ice_latlon = pd.read_excel(obs, name)
+    lat = ice_latlon[name].iloc[0].split(',')[0]
+    lon = ice_latlon[name].iloc[0].split(',')[1]
+    print(lon)
+    #newlat = dms2deg(lat)
+    #newlon = dms2deg(lon)
+    #print(lon, newlon)
+
+    ice_data = pd.read_excel(obs, name, skiprows=2)
+    if 'so' in var:
+        ice = ice_data['total SO42- (ppb = mg/l)']
+    else:
+        ice = ice_data['BC (ppb)']
+    ice_year = ice_data.iloc[:,0]
+
+    #print(ice)
+
+def find_mcconnell_icecore(name, var):
+    # 
+    #
+    # Ex: data, year, lat, lon = find_mcconnell_icecore('Ngreen', 's')
+    #--------------------------------------------------------------------
+    obs = pd.ExcelFile('/cluster/home/krisomos/ice_cores/data/Greenland_data_for_Kine_051021.xlsx')
+    ice = pd.read_excel(obs,'Concentration_All_1750_2012')
+    icename = pd.read_excel(obs,'Core Stats')
+    
+    icelat = []
+    icelon = []
+    ice_data = []
+    if "Sgreen" in name:
+        namelist = ['ACT2', 'ACT11D', 'D4', 'Summit2010'] 
+    elif "Ngreen" in name:
+        namelist= ['NGT_B19', 'Tunu2013', 'NEEM_2011_S1',  'Humboldt']
+    else:
+        namelist=[name]
+
+    if 'bc' in var:
+        newnames = [s + '_BC_ng_g' for s in namelist]
+    elif 's' in var:
+        newnames = [s + '_nssS_ng_g' for s in namelist]
+
+
+    for i in range(len(namelist)): 
+        hei = icename[icename['Core'].isin([namelist[i]])]
+        ice_lat = hei['Lat'].values[0]
+    
+        ice_lon = hei['Lon'].values[0]
+        if ice_lon < 0:
+            ice_lon = ice_lon+360
+        else:
+            print(ice_lon)
+        
+        icelat.append(ice_lat)
+        icelon.append(ice_lon)
+
+        icedata = ice[newnames[i]].values
+        ice_data.append(icedata)
+    
+    
+    ice_year = ice['Mid Year'].values
+    
+    ice_data = np.nanmean(ice_data,axis=0) #Note that by using nanmean we ignore the icecores with nan values, so some years have a mean based on fewer sites
+
+    return ice_data[::-1], ice_year[::-1], icelat, icelon
+    
+def lookup(fullpath, var):
+    list_of_data = []
+    for file in glob.glob(fullpath):
+        print(file)
+        opendata = xr.open_dataset(file)[var]
+        list_of_data.append(opendata)
+    if len(list_of_data) > 1:
+        #print('More than one file per model and exp - must concatenate')
+        concatted = xr.concat(list_of_data,dim='time')
+        opendata = concatted.sortby('time')
+    elif len(list_of_data)==0:
+        print('NO MATCHES FOR THIS PATH', fullpath)
+        sys.exit()
+    else:
+        opendata = list_of_data[0]
+    return opendata
+
+def model_concentration_timeseries(model, var, lati, longi, greenland=False):
+    from pathfinder import pathfinder
+    from pathfinder import pr_pathfinder
+
+    if 'NorESM' in model:
+        path = '/cluster/home/krisomos/noresmdata/'  #This folder needs to be sshfs-ed
+    else:
+        path = '/trd-project1/' # Betzy
+
+    histwetpath, histdrypath, ctrlwetpath, ctrldrypath = pathfinder(model,var)
+    histprpath, ctrlprpath, areafile                   = pr_pathfinder(model)
+    wet = 'wet'+var
+    dry = 'dry'+var
+    
+    # Create one sorted timeseries per variable in Q 
+    vaat      = lookup(path+histwetpath,wet)
+    #vaat_ctrl = lookup(path+ctrlwetpath,wet)
+    torr      = lookup(path+histdrypath,dry)
+    #torr_ctrl = lookup(path+ctrldrypath,dry)
+    pr        = lookup(path+histprpath,'pr')
+    #pr_ctrl   = lookup(path+ctrlprpath,'pr')
+
+    area      = xr.open_dataset(path+areafile)['areacella']
+
+    # Calculate the concentration from the historical experiment and change unit to kg
+    area = find_3x3matrix(area,lati,longi,True)                 # m2
+
+    torr = find_3x3matrix(torr, lati,longi)                     # kg m-2 s-1
+    torr = torr*area.values*(365*24*60*60)                      # kg
+
+    vaat = find_3x3matrix(vaat, lati,longi)                     # kg m-2 s-1
+    vaat = vaat*area.values*(365*24*60*60)                      # kg
+
+    prec = find_3x3matrix(pr,lati,longi)                        # kg m-2 s-1
+    prec = prec*area.values*(365*24*60*60)                      # kg
+
+    tot_dep = np.absolute(torr) + np.absolute(vaat)             # kg
+    conc = (tot_dep/prec)*(1E12/1E3)                            # kg/kg --> ng/g
+
+    """
+    # Same for piControl
+    torr_ctrl = find_3x3matrix(torr_ctrl, lati,longi)           # kg m-2 s-1
+    torr_ctrl = torr_ctrl*area.values*(365*24*60*60)            # kg
+
+    vaat_ctrl = find_3x3matrix(vaat_ctrl, lati,longi)           # kg m-2 s-1
+    vaat_ctrl = vaat_ctrl*area.values*(365*24*60*60)            # kg
+
+    prec_ctrl = find_3x3matrix(pr_ctrl,lati,longi)              # kg m-2 s-1
+    prec_ctrl = prec_ctrl*area.values*(365*24*60*60)            # kg
+
+    tot_dep_ctrl = np.absolute(torr_ctrl) + np.absolute(vaat_ctrl)    # kg
+    conc_ctrl = (tot_dep_ctrl/prec_ctrl)*(1E12/1E3)                   # kg/kg --> ng/g
+    conc_ctrl = np.mean(conc_ctrl)
+
+    # Find concentration anomaly
+    dConc = conc - conc_ctrl
+
+    out = xr.DataArray(dConc,name='conc_anomaly')
+    out['conc_absolute'] = xr.DataArray(conc)
+    out['conc_ctrl'] = conc_ctrl
+    print(type(out))
+    print(out)
+    """
+    out = xr.DataArray(conc, name='Conc_Absolute')
+    return out
+
+def write_netcdf(name, var):
+    data, year, lat, lon = find_mcconnell_icecore(name,var)
+    models =  ['CNRM-ESM2-1','CESM2','GFDL-ESM4','CanESM5','GISS-E2-1-H','GISS-E2-1-G','CESM2-WACCM','EC-Earth3-AerChem'] #'NorESM2-LM','MPI-ESM-1-2-HAM','INM-CM4-8', 'INM-CM5-0',
+    
+    for j in range(len(models)):
+        if len(lat)>1:
+            outlist = []
+            for i in range(len(lat)):
+                outfile = model_concentration_timeseries(models[j],var,lat[i],lon[i],True)
+                outlist.append(outfile.values)
+            #print(outlist)
+            outfile = xr.DataArray(np.mean(outlist,axis=0), name='Conc_Absolute')
+
+            print(outfile)
+        else:
+            outfile = model_concentration_timeseries(models[j],var,lat,lon)
+
+        outfile.to_netcdf('/cluster/home/krisomos/ice_cores/output/'+var+'_'+models[j]+'_'+name+'_conc.nc')
+        #conc.to_netcdf('/cluster/home/krisomos/ice_cores/output/'+var+'_'+model+'_'+name+'_conc.nc')
+
+areas = ['Ngreen','Sgreen','ACT11D','ACT2','NGT_B19','Tunu2013','NEEM_2011_S1','Humboldt','Summit2010','D4']
+for k in range(len(areas)):
+    write_netcdf(areas[k],'so4')
+    write_netcdf(areas[k],'bc')
--- ice_cores/pathfinder.py
+++ ice_cores/pathfinder.py
@@ -7,7 +7,7 @@ Function to find right variables for ice core study for a selection of models in
 """
 # on betzy: /trd-project1/NS9252K/ESGF/CMIP6/CMIP
 
-def pathfinder(model,var, pr=False):
+def pathfinder(model,var):
     if 'GISS-E2-1-' in model:
         # Both models have pr and bc as below
         histwet = 'NS9252K/ESGF/CMIP6/CMIP/NASA-GISS/'+model+'/historical/r1i1p3f1/AERmon/wet'+var+'/gn/v20*/*'
@@ -15,7 +15,8 @@ def pathfinder(model,var, pr=False):
         ctrlwet = 'NS9252K/ESGF/CMIP6/CMIP/NASA-GISS/'+model+'/piControl/r1i1p3f1/AERmon/wet'+var+'/gn/v20*/*'
         ctrldry = 'NS9252K/ESGF/CMIP6/CMIP/NASA-GISS/'+model+'/piControl/r1i1p3f1/AERmon/dry'+var+'/gn/v20*/*'
     elif 'EC-Earth3-AerChem' in model:
-        # This model does not have bc nor pr
+        # This model has bc as below
+        # This model has pr in similar path but grid is "gr" 
         histwet = 'NS9252K/ESGF/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3-AerChem/historical/r1i1p1f1/AERmon/wet'+var+'/gn/v*/*'
         histdry = 'NS9252K/ESGF/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3-AerChem/historical/r1i1p1f1/AERmon/dry'+var+'/gn/v*/*'
         ctrlwet = 'NS9252K/ESGF/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3-AerChem/piControl/r1i1p1f1/AERmon/wet'+var+'/gn/v*/*'
@@ -61,16 +62,52 @@ def pathfinder(model,var, pr=False):
     elif 'NorESM2' in model:
         # Both has pr as below
         # MM is missing both so4 and bc in piControl
-        histwet = 'CMIP6/CMIP/NCC/'+model+'/historical/r1i1p1f1/AERmon/wet'+var+'/gn/v*/*'
-        histdry = 'CMIP6/CMIP/NCC/'+model+'/historical/r1i1p1f1/AERmon/dry'+var+'/gn/v*/*'
-        ctrlwet = 'CMIP6/CMIP/NCC/'+model+'/piControl/r1i1p1f1/AERmon/wet'+var+'/gn/v*/*'
-        ctrldry = 'CMIP6/CMIP/NCC/'+model+'/piControl/r1i1p1f1/AERmon/dry'+var+'/gn/v*/*'
+        histwet = 'CMIP6/CMIP/NCC/'+model+'/historical/r1i1p1f1/AERmon/wet'+var+'/gn/latest/*'
+        histdry = 'CMIP6/CMIP/NCC/'+model+'/historical/r1i1p1f1/AERmon/dry'+var+'/gn/latest/*'
+        ctrlwet = 'CMIP6/CMIP/NCC/'+model+'/piControl/r1i1p1f1/AERmon/wet'+var+'/gn/latest/*'
+        ctrldry = 'CMIP6/CMIP/NCC/'+model+'/piControl/r1i1p1f1/AERmon/dry'+var+'/gn/latest/*'
     
-    if pr==True:
-        print('ojoj')
+    return histwet, histdry, ctrlwet, ctrldry
 
-    else:
-        return histwet, histdry, ctrlwet, ctrldry
+
+def pr_pathfinder(model):
+    if 'GISS-E2-1-' in model:
+        histpr = 'NS9252K/ESGF/CMIP6/CMIP/NASA-GISS/'+model+'/historical/r1i1p3f1/Amon/pr/gn/v20*/*'
+        ctrlpr = 'NS9252K/ESGF/CMIP6/CMIP/NASA-GISS/'+model+'/piControl/r1i1p3f1/Amon/pr/gn/v20*/*'
+        area   = 'NS9252K/ESGF/CMIP6/CMIP/NASA-GISS/'+model+'/piControl/r1i1p3f1/fx/areacella/gn/latest/areacella_fx_'+model+'_piControl_r1i1p3f1_gn.nc'
+    elif 'EC-Earth3-AerChem' in model:
+        histpr = 'NS9252K/ESGF/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3-AerChem/historical/r1i1p1f1/Amon/pr/gr/v*/*'
+        ctrlpr = 'NS9252K/ESGF/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3-AerChem/piControl/r1i1p1f1/Amon/pr/gr/v*/*'
+        area   = 'NS9252K/ESGF/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3-AerChem/historical/r1i1p1f1/fx/areacella/gr/latest/areacella_fx_EC-Earth3-AerChem_historical_r1i1p1f1_gr.nc'
+    elif 'MPI-ESM-1-2-HAM' in model:
+        histpr = 'NS9252K/ESGF/CMIP6/CMIP/HAMMOZ-Consortium/MPI-ESM-1-2-HAM/historical/r1i1p1f1/Amon/pr/gn/v*/*'
+        ctrlpr = 'NS9252K/ESGF/CMIP6/CMIP/HAMMOZ-Consortium/MPI-ESM-1-2-HAM/piControl/r1i1p1f1/Amon/pr/gn/v*/*' 
+        area   =  'NS9252K/ESGF/CMIP6/CMIP/HAMMOZ-Consortium/MPI-ESM-1-2-HAM/historical/r1i1p1f1/fx/areacella/gn/latest/areacella_fx_MPI-ESM-1-2-HAM_historical_r1i1p1f1_gn.nc'
+    elif 'CESM2' in model:
+        histpr = 'NS9252K/ESGF/CMIP6/CMIP/NCAR/'+model+'/historical/r1i1p1f1/Amon/pr/gn/v*/*.nc'
+        ctrlpr = 'NS9252K/ESGF/CMIP6/CMIP/NCAR/'+model+'/piControl/r1i1p1f1/Amon/pr/gn/v*/*.nc'
+        area   = 'NS9252K/ESGF/CMIP6/CMIP/NCAR/'+model+'/historical/r1i1p1f1/fx/areacella/gn/latest/areacella_fx_'+model+'_historical_r1i1p1f1_gn.nc'
+    elif 'CanESM5' in model:
+        histpr = 'NS9252K/ESGF/CMIP6/CMIP/CCCma/CanESM5/historical/r1i1p1f1/Amon/pr/gn/latest/*'
+        ctrlpr = 'NS9252K/ESGF/CMIP6/CMIP/CCCma/CanESM5/piControl/r1i1p1f1/Amon/pr/gn/latest/*'
+        area = 'NS9252K/ESGF/CMIP6/CMIP/CCCma/CanESM5/historical/r1i1p1f1/fx/areacella/gn/latest/areacella_fx_CanESM5_historical_r1i1p1f1_gn.nc'
+    elif 'INM-CM' in model:
+        histpr = 'NS9252K/ESGF/CMIP6/CMIP/INM/'+model+'/historical/r1i1p1f1/Amon/pr/gr1/v*/*'
+        ctrlpr = 'NS9252K/ESGF/CMIP6/CMIP/INM/'+model+'/piControl/r1i1p1f1/Amon/pr/gr1/v*/*'
+        area   = 'NS9252K/ESGF/CMIP6/CMIP/INM/'+model+'/piControl/r1i1p1f1/fx/areacella/gr1/latest/areacella_fx_'+model+'_historical_r1i1p1f1_gr1.nc'
+    elif 'GFDL-ESM4' in model:
+        histpr = 'NS9252K/ESGF/CMIP6/CMIP/NOAA-GFDL/GFDL-ESM4/historical/r1i1p1f1/Amon/pr/gr1/v*/*'
+        ctrlpr = 'NS9252K/ESGF/CMIP6/CMIP/NOAA-GFDL/GFDL-ESM4/piControl/r1i1p1f1/Amon/pr/gr1/v*/*'
+        area   =  'NS9252K/ESGF/CMIP6/CMIP/NOAA-GFDL/GFDL-ESM4/historical/r1i1p1f1/fx/areacella/gr1/latest/areacella_fx_GFDL-ESM4_historical_r1i1p1f1_gr1.nc'
+    elif 'CNRM-ESM2-1' in model:
+        histpr = 'NS9252K/ESGF/CMIP6/CMIP/CNRM-CERFACS/CNRM-ESM2-1/historical/r1i1p1f2/Amon/pr/gr/v*/*'
+        ctrlpr = 'NS9252K/ESGF/CMIP6/CMIP/CNRM-CERFACS/CNRM-ESM2-1/piControl/r1i1p1f2/Amon/pr/gr/v*/*'
+        area   = 'NS9252K/ESGF/CMIP6/CMIP/CNRM-CERFACS/CNRM-CM6-1/historical/r1i1p1f2/fx/areacella/gr/latest/areacella_fx_CNRM-CM6-1_historical_r1i1p1f2_gr.nc'
+    elif 'NorESM2' in model:
+        histpr = 'CMIP6/CMIP/NCC/'+model+'/historical/r1i1p1f1/Amon/pr/gn/latest/*'
+        ctrlpr = 'CMIP6/CMIP/NCC/'+model+'/piControl/r1i1p1f1/Amon/pr/gn/latest/*'
+        area   = 'CMIP6/CMIP/NCC/NorESM2-LM/historical/r1i1p1f1/fx/areacella/gn/latest/areacella_fx_NorESM2-LM_historical_r1i1p1f1_gn.nc'
+    return histpr, ctrlpr, area
 
 """
         histwet = '/historical/r1i1p1f1/AERmon/wet'+var+'/gn/v*/*'
